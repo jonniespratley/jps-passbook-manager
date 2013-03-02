651d7d988001c34090c327e60140313457c586f5
Jonnie Spratley <jonniespratley@gmail.com>
2013-03-02 10:00:29 -0800

    updated flow

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,7 +4,7 @@ jpsPassbookManagerApp.controller('MainCtrl', function($scope, $rootScope, $http,
     $rootScope.location = $location;
 	$rootScope.App = {
 		name: 'Pass Manager',
-		icon: 'tags',
+		icon: 'edit',
 		hero:{
 		  title: 'Easy Passes',
 		  body: 'With this interface you can easily create Apple iOS Passbook Passes.'
@@ -12,7 +12,7 @@ jpsPassbookManagerApp.controller('MainCtrl', function($scope, $rootScope, $http,
 		menu: [
 			{ id: null, slug: 'home', title: 'Home', icon: 'home', href:'#/home' },
 		//	{ id: null, slug: 'manage', title: 'Manage', icon: 'edit', href:'#/manage' },
-			{ id: null, slug: 'passes', title: 'Passes', icon: 'edit', href:'#/passes' },
+			{ id: null, slug: 'passes', title: 'Passes', icon: 'tags', href:'#/passes' },
 			{ id: null, slug: 'server', title: 'Server', icon: 'cloud', href:'#/server' },
 			{ id: null, slug: 'docs', title: 'Docs', icon: 'book', href:'#/docs' }
 		]
