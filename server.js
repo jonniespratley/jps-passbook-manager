1a35151e9827b1918409d8033a1ec769da6f1ae4
Jonnie Spratley <jonniespratley@gmail.com>
2016-01-05 12:06:48 -0800

    fix: removed pouchdb

--- server.js
+++ server.js
@@ -6,14 +6,12 @@
 const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
-const PouchDB = require('pouchdb');
 const debug = require('debug');
-
 const fs = require('fs-extra');
 
 const config = require(path.resolve(__dirname, './config.json'));
 const port = process.env.PORT || config.server.port || null;
-const host = process.env.VCAP_APP_HOST || config.server.hostname || null;
+const host = process.env.VCAP_APP_HOST || process.env.IP || config.server.hostname || null;
 
 var app = express();
 app.use('/', serveStatic(path.resolve(__dirname, './app')));
@@ -21,7 +19,6 @@ app.use('/public', serveStatic(path.resolve(__dirname, './www')));
 
 var program = require('./lib/program')(config);
 program.app = app;
-
 
 var middleware = [
 	path.resolve(__dirname, './routes/jps-passbook-routes')
