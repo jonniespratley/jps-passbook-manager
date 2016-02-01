f617b642582b1f25fff1216ebc3ab7a875dc26a1
Jonathan Spratley <jonniespratley@gmail.com>
2016-02-01 08:45:00 -0800

    fix: added test identifer to mocks

--- test/helpers/mocks.js
+++ test/helpers/mocks.js
@@ -4,19 +4,23 @@ var config = require(path.resolve(__dirname, '../../config.js'));
 var program = require(path.resolve(__dirname, '../../lib/program.js'))({
 	dataPath: path.resolve(__dirname, '../temp')
 });
-exports.program = program;
+
 
 var Pass = require(path.resolve(__dirname, '../../lib/models/pass.js'));
 var Passes = require(path.resolve(__dirname, '../../lib/models/passes.js'));
 var Device = require(path.resolve(__dirname, '../../lib/models/device.js'));
 
 exports.mockIdentifer = {
-	passTypeIdentifier: 'pass.io.passbookmanager',
+	passTypeIdentifier: 'pass.io.passbookmanager.test',
 	wwdr: path.resolve(__dirname, '../../certificates/wwdr-authority.pem'),
-	p12: path.resolve(__dirname, '../../certificates/pass.io.passbookmanager.p12'),
-	passphrase: 'fred'
+	p12: path.resolve(__dirname, '../../certificates/pass.io.passbookmanager.test.p12'),
+	passphrase: 'test'
 };
+program.config.defaults.passkit.passTypeIdentifier = exports.mockIdentifer.passTypeIdentifier;
+console.log('MOCK program', program.config.defaults);
 
+
+exports.program = program;
 
 exports.mockPasses = [
 
@@ -56,7 +60,7 @@ exports.mockPasses = [
 exports.mockPass = exports.mockPasses[0];
 
 ///api/v1/v1/devices/a53ae770f6bd12d04c572e653888c6c6/registrations/pass.passbookmanager.io/25df3392-f37d-48c3-a0a1-20e9edc95f8b
-const mockDevice = new Device({
+exports.mockDevice = new Device({
 	//_id: 'device-a53ae770f6bd12d04c572e653888c6c6',
 	pushToken: 'ce0a5983ba7e600416d5da202cf9c218050fd424581ea259bc01174238b5a9d2',
 	deviceLibraryIdentifier: '1234567890',
@@ -64,4 +68,3 @@ const mockDevice = new Device({
 	authorization: 'ApplePass 0123456789876543210',
 	passTypeIdentifier: exports.mockPass.passTypeIdentifier
 });
-exports.mockDevice = mockDevice;
