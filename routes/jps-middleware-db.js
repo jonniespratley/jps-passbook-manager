2a214738b28957530d299a1e4bf357a7219dd6bd
Jonnie Spratley <jonniespratley@gmail.com>
2016-02-04 08:15:53 -0800

    fix: refactor out server

--- routes/jps-middleware-db.js
+++ routes/jps-middleware-db.js
@@ -1,25 +1,25 @@
 'use strict';
 
-module.exports = function(config, app, Router, Logger, DbController) {
+module.exports = function(config, Logger, DbController) {
+
+	const Router = require('express').Router;
 	const bodyParser = require('body-parser');
 
 	let logger = Logger.getLogger('router:db')
 	let dbRouter = new Router();
-	let dbController = new DbController(program);
+
 
 	dbRouter.route('/:id?', bodyParser.json()).all(function(req, res, next) {
 			logger('debug', req.method, req.url);
 			logger('all route', req.method, req.url, req.params)
 			next();
 		})
-		.get(dbController.get_doc)
-		.delete(dbController.delete_doc)
-		.put(dbController.put_doc)
-		.post(dbController.post_doc);
+		.get(DbController.get_doc)
+		.delete(DbController.delete_doc)
+		.put(DbController.put_doc)
+		.post(DbController.post_doc);
 
 	dbRouter.use(bodyParser.json());
-
-	app.use('/api/' + config.version + '/db', dbRouter);
 
 	return dbRouter;
 };
