bf82b20bfb0be7232c167115e2d1804e093da2c7
Jonnie Spratley <jonniespratley@gmail.com>
2016-02-05 00:06:32 +0000

    fix: route

--- routes/jps-middleware-db.js
+++ routes/jps-middleware-db.js
@@ -6,7 +6,7 @@ module.exports = function(DbController, Logger) {
 
 	let logger = Logger.getLogger('router:db');
 	let dbRouter = new Router();
-
+	dbRouter.use(bodyParser.json());
 	dbRouter.route('/:id?', bodyParser.json()).all(function(req, res, next) {
 			logger('all route', req.method, req.url, req.params);
 			next();
@@ -15,8 +15,6 @@ module.exports = function(DbController, Logger) {
 		.delete(DbController.delete_doc)
 		.put(DbController.put_doc)
 		.post(DbController.post_doc);
-
-	dbRouter.use(bodyParser.json());
 
 	return dbRouter;
 };
