f9c9820c60ec96ddc5ac7a94b8d2d229154923a6
Jonnie Spratley <jonnie.spratley@ge.com>
2016-02-05 09:31:19 -0800

    fix: updated services

--- routes/jps-middleware-devices.js
+++ routes/jps-middleware-devices.js
@@ -1,23 +1,19 @@
 'use strict';
-var express = require('express'),
-	bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 module.exports = function(DevicesController) {
-	return function(){
-
-
-		let router = new express.Router();
-		router.get('/:device_id/push/:token', function(req, res) {
-			logger('Push to device ' + req.params.token);
-			res.status(200).send({
-				message: 'Device push token'
-			});
+	var router = new express.Router();
+	router.get('/:device_id/push/:token', function(req, res) {
+		logger('Push to device ' + req.params.token);
+		res.status(200).send({
+			message: 'Device push token'
 		});
+	});
 
-		router.get('/:device_id/registrations/:pass_type_id?', DevicesController.get_device_passes);
-		router.post('/:device_id/registrations/:pass_type_id/:serial_number', bodyParser.json(), DevicesController.post_device_registration);
-		router.delete('/:device_id/registrations/:pass_type_id/:serial_number', DevicesController.delete_device_registration);
+	router.get('/:device_id/registrations/:pass_type_id?', DevicesController.get_device_passes);
+	router.post('/:device_id/registrations/:pass_type_id/:serial_number', bodyParser.json(), DevicesController.post_device_registration);
+	router.delete('/:device_id/registrations/:pass_type_id/:serial_number', DevicesController.delete_device_registration);
 
-		return router;
-	};
+	return router;
 };
