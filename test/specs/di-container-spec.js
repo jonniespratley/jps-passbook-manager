1180492d8817586c512bb62f0e1517e6f20835ba
Jonnie Spratley <jonniespratley@gmail.com>
2016-02-07 18:31:30 -0800

    fix: cleanup

--- test/specs/di-container-spec.js
+++ test/specs/di-container-spec.js
@@ -1,11 +1,9 @@
 'use strict';
-
 const assert = require('assert');
 const path = require('path');
 
-
-//var mocks = require(path.resolve(__dirname, '../helpers/mocks'));
 const DIContainer = require(path.resolve(__dirname, '../../lib/di-container'));
+//var mocks = require(path.resolve(__dirname, '../helpers/mocks'));
 //var program = mocks.program;
 //var config = program.get('config');
 
@@ -20,12 +18,13 @@ var container = null;
 const ApiFactory = function (config) {
 	class LocalApi {
 		constructor(c) {
-			console.warn('You injected', c);
+			console.warn('LocalApi.constructor', c);
 		}
 
 		post(obj) {
 			console.warn('post', obj);
 		}
+
 		get(obj) {
 			console.warn('get', obj);
 		}
@@ -36,24 +35,28 @@ const ApiFactory = function (config) {
 const DbService = function (config, someModule) {
 	class LocalStore {
 		constructor(c) {
-			console.warn('You injected', c);
+			console.warn('LocalStore.constructor', c);
 		}
-		put(obj){
+
+		put(obj) {
 			console.warn('put', obj);
 		}
-		allDocs(o){
+
+		allDocs(o) {
 			console.warn('allDocs', o);
 		}
-		remove(obj){
+
+		remove(obj) {
 			console.warn('save', obj);
 		}
+
 		get(id) {
 			console.warn('Find by', id);
 		}
 	}
 	return new LocalStore(config);
+	//return LocalStore;
 };
-
 
 describe('DIContainer', function () {
 	before(function () {
@@ -66,15 +69,9 @@ describe('DIContainer', function () {
 		done();
 	});
 
-	it('register/value(name, value) - should register module by name', function (done) {
-		assert(container.value);
-		container.value('someModule', {name: 'value'});
-		done();
-	});
-
-	it('get(name) - should return registered module by name', function (done) {
-		assert(container.get);
-		assert(container.get('someModule').name === 'value');
+	it('register(name, value) - should register module by name', function (done) {
+		assert(container.register);
+		container.register('someModule', {name: 'value'});
 		done();
 	});
 
@@ -84,21 +81,62 @@ describe('DIContainer', function () {
 		done();
 	});
 
+	it('service(name, prototype) - should register service by name', function (done) {
+		assert(container.service);
+		container.service('Db', new DbService());
+		done();
+	});
+
+	it('get(name) - should return registered module by name', function (done) {
+		assert(container.get);
+		assert(container.get('someModule').name === 'value');
+		done();
+	});
+
 	it('get(factory) - should return instance of factory', function (done) {
 		assert(container.get('Api').post);
 		done();
 	});
 
-	it('service(name, prototype) - should register service constructor by name', function (done) {
-		assert(container.service);
-		container.factory('Db', DbService);
-		done();
-	});
-
-	it('get(service) - should return new instance of service', function (done) {
+	it('get(name) - should return new instance of service', function (done) {
 		assert(container.get('Db').get);
 		done();
 	});
 
+	it('get(name) - should throw error if service does not exist', function (done) {
+		assert.throws(function () {
+			container.get('MyApi');
+		}, Error);
+		done();
+	});
+
+	it('inject(service) - inject dependencies into service', function (done) {
+		assert(container.inject(DbService));
+		done();
+	});
+
+	it('modules - should contain Map of dependencies', function (done) {
+		assert(container.modules);
+		assert(container.modules.get);
+		assert(container.modules.set);
+		assert(container.modules.has);
+		assert(container.modules.keys);
+		assert(container.modules.values);
+		assert(container.modules instanceof Map);
+		done();
+	});
+
+	it('modules.has() - should return true if module exists', function (done) {
+		assert(container.modules.has('Api') === true);
+		assert(container.modules.has('Unknown') === false);
+		done();
+	});
+
+	it('modules.keys() - should return all module keys', function (done) {
+		var keys = container.modules.keys();
+		console.log(keys);
+		assert(keys);
+		done();
+	});
 
 });
